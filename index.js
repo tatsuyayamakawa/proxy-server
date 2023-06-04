64f9ae9b696e71b10f56e5d1bee2451e7be77d72
tatsuyayamakawa <t.yamakawa0121@gmail.com>
2023-06-04 09:54:13 +0900

    add: weatherAPIの追加 #002

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+const url = require("url")
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
@@ -13,6 +14,17 @@ app.use(limiter);
 
 app.get("/", (req, res) => {
 	res.send("This is my proxy server");
+})
+
+app.use("/weather-data", (req, res, next) => {
+	const city = new URL(req.url).query
+	createProxyMiddleware({
+		target: `${process.env.BASE_API_URL_WEATHERAPI}${city}&api=no`,
+		changeOrigin: true,
+		pathRewrite: {
+			[`^/weather-data`]: "",
+		},
+	})(req, res, next)
 })
 
 app.use("/corona-tracker-world-data", (req, res, next) => {
